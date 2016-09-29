0d05b66a24eabc6bc9ab49f405f370cb5ea51b86
winslowism <mpkonstmann@hotmail.com>
2016-09-29 11:12:55 +0200

    Added ItemGroup methods

--- Kode/DatabaseAPI/DatabaseAPI/TableItemGroup/SqlTableItemGroup.cs
+++ Kode/DatabaseAPI/DatabaseAPI/TableItemGroup/SqlTableItemGroup.cs
@@ -1,22 +1,92 @@
-﻿using DatabaseAPI.DatabaseModel;
+﻿using System;
+using System.Data.SqlClient;
+using DatabaseAPI.DatabaseModel;
 
 namespace DatabaseAPI.TableItemGroup
 {
     public class SqlTableItemGroup : ITableItemGroup
     {
-        public void CreateItemGroup(string itemGroupItem, int itemGroupParentID)
+        private SqlConnection _connection;
+        private SqlCommand _command;
+        private SqlDataReader _dataReader;
+
+
+        public SqlTableItemGroup()
         {
-            throw new System.NotImplementedException();
+            _connection = new SqlConnection("Poops and things");
+        }
+
+        public void CreateItemGroup(string itemGroupName, int itemGroupParentID)
+        {
+            try
+            {
+                _connection.Open();
+
+                _command =
+                    new SqlCommand(
+                        $"INSERT INTO ItemGroup (Name, ParentItemGroupID) VALUES ({itemGroupName},{itemGroupParentID}",
+                        _connection);
+
+                _command.ExecuteNonQuery();
+            }
+            finally
+            {
+                _connection?.Close();
+            }
         }
 
         public void DeleteItemGroup(int itemGroupID)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                _connection.Open();
+
+                _command = new SqlCommand($"DELETE FROM ItemGroup WHERE ItemGroupID = {itemGroupID}", _connection);
+
+                _command.ExecuteNonQuery();
+
+            }
+            finally
+            {
+                _connection?.Close();
+
+            }
         }
 
         public ItemGroup GetItemGroup(int itemGroupID)
         {
-            throw new System.NotImplementedException();
+            ItemGroup ItemGroupResult;
+            string itemGroupName;
+            string itemGroupParentID;
+
+            try
+            {
+                _connection.Open();
+
+                _command = new SqlCommand($"SELECT FROM ItemGroup WHERE ItemGroupID = {itemGroupID}",_connection);
+
+                _dataReader = _command.ExecuteReader();
+
+                if (_dataReader.HasRows)
+                {
+                    while (_dataReader.Read())
+                    {
+                        itemGroupName = (string) _dataReader["Name"];
+                        itemGroupParentID = (string) _dataReader["ParentItemGroupID"];
+
+
+                    }
+                }
+                ItemGroupResult = new ItemGroup(itemGroupName, itemGroupParentID, itemGroupID);
+            }
+            finally 
+            {
+                _dataReader?.Close();
+                _connection?.Close();
+                
+            }
+
+            return ItemGroupResult;
         }
     }
 }
