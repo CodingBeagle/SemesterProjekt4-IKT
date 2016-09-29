13d2937815b8e73df7aca148ce083d96071c9e26
Vestergaard <Vestergaard32@gmail.com>
2016-09-29 09:49:48 +0200

    Started implementation on SQLTableItem

--- Kode/DatabaseAPI/DatabaseAPI/TableItem/SqlTableItem.cs
+++ Kode/DatabaseAPI/DatabaseAPI/TableItem/SqlTableItem.cs
@@ -1,7 +1,23 @@
-﻿namespace DatabaseAPI.TableItem
+﻿using System.Collections.Generic;
+using DatabaseAPI.DatabaseModel;
+
+namespace DatabaseAPI.TableItem
 {
-    public class SqlTableItem
+    public class SqlTableItem : ITableItem
     {
-        
+        public void CreateItem(string name, int itemGroup)
+        {
+            string SQLInsertCommand = $"INSERT INTO Item";
+        }
+
+        public void DeleteItem(int ID)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Item> SearchItems(string itemName)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
