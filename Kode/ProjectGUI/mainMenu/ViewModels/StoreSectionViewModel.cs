d155756ce07a2ef4398cc126b64f634b0482e320
winslowism <mpkonstmann@hotmail.com>
2016-10-17 15:45:08 +0200

    Removed unused events

--- Kode/ProjectGUI/mainMenu/ViewModels/StoreSectionViewModel.cs
+++ Kode/ProjectGUI/mainMenu/ViewModels/StoreSectionViewModel.cs
@@ -168,14 +168,6 @@ namespace mainMenu
             }
 
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-
+        
     }
 }
