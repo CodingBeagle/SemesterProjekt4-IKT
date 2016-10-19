fdc47b32da27d5aa17f50a81a006665312d2dcd5
CodingBeagle <kasper.rie@gmail.com>
2016-10-19 11:21:48 +0200

    Removed unnecessary using statements from Database API classes

--- Kode/DatabaseAPI/DatabaseAPI/TableFloorplan/SqlTableFloorplan.cs
+++ Kode/DatabaseAPI/DatabaseAPI/TableFloorplan/SqlTableFloorplan.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using DatabaseAPI.DatabaseModel;
+using System.IO;     
 
 namespace DatabaseAPI.TableFloorplan
 {
