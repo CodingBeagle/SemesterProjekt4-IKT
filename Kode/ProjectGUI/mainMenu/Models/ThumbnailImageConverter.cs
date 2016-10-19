07a5b6f8f8105088636bf954165ad587f4131929
CodingBeagle <kasper.rie@gmail.com>
2016-10-19 09:47:06 +0200

    Refactored value converter to use RefreshableImage class

--- Kode/ProjectGUI/mainMenu/Models/ThumbnailImageConverter.cs
+++ Kode/ProjectGUI/mainMenu/Models/ThumbnailImageConverter.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Net.Cache;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
-using System.Windows.Media.Imaging;
+using mainMenu.RefreshLogic;
 
 namespace mainMenu.Models
 {
@@ -20,18 +15,9 @@ namespace mainMenu.Models
                 string filePath = value.ToString();
                 if (File.Exists(filePath))
                 {
-                    BitmapImage result = new BitmapImage();
-                    result.BeginInit();
-                    result.UriSource = new Uri(filePath, UriKind.Relative);
-                    // .OnLoad makes sure WPF prevents keeping a lock on the file
-                    result.CacheOption = BitmapCacheOption.OnLoad;
-                    // .IgnoreImageCache causes WPF to reread the image every time
-                    // Should be used when selected images needs to be refreshed
-                    result.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
-                    result.EndInit();
-                    return result;
+                    RefreshableImage refreshableImage = new RefreshableImage();
+                    return refreshableImage.Get(filePath);
                 }
-
             }
 
             return null;
