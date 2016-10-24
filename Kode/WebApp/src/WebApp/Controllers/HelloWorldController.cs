8201f724a183855e9d97b68b430637af1ba7c9b1
Christoffersh <201407641@post.au.dk>
2016-10-24 13:28:30 +0200

    Added HelloWorld controller and view for test purposes. Installed EntityFramework to make ready for database model extraction

---
+++ Kode/WebApp/src/WebApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WebApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int num = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["Num"] = num;
+            return View();
+        }
+    }
+}
