cd846975846f4e823d3b0fd1cfcb798ff31c913a
CodingBeagle <kasper.rie@gmail.com>
2016-11-03 11:13:48 +0100

    Fixed integration test project settings

--- Kode/Test/DatabaseAPI.StoreDatabase.Integration/DatabaseAPI.StoreDatabase.Integration/Step1TableItemIntegration.cs
+++ Kode/Test/DatabaseAPI.StoreDatabase.Integration/DatabaseAPI.StoreDatabase.Integration/Step1TableItemIntegration.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DatabaseAPI.Factories;
 using NUnit.Framework;
 
 namespace DatabaseAPI.StoreDatabase.Integration
 {
-    class Step1TableItemIntegration
+    [TestFixture]
+    public class Step1TableItemIntegration
     {
+        private IDatabaseService _db;
+
         [SetUp]
         public void Setup()
-        {
-            
+        {   
+            _db = new DatabaseService(new SqlStoreDatabaseFactory());
         }
+
+
     }
 }
