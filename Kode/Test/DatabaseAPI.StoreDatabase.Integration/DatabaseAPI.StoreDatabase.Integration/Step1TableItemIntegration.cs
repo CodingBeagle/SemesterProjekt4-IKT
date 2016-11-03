2c5533773d123a06e03fce016bdabe506983ca59
CodingBeagle <kasper.rie@gmail.com>
2016-11-03 11:08:40 +0100

    Added first test fixture for Database API integration

---
+++ Kode/Test/DatabaseAPI.StoreDatabase.Integration/DatabaseAPI.StoreDatabase.Integration/Step1TableItemIntegration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DatabaseAPI.StoreDatabase.Integration
+{
+    class Step1TableItemIntegration
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+    }
+}
