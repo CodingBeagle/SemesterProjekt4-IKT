7c04c49105499ab9ba8995317ad1f6a65dd4947e
CodingBeagle <kasper.rie@gmail.com>
2016-11-07 08:04:18 +0100

    Integration testing

--- Kode/Test/DatabaseAPI.StoreDatabase.Integration/DatabaseAPI.StoreDatabase.Integration/Step1TableItemIntegration.cs
+++ Kode/Test/DatabaseAPI.StoreDatabase.Integration/DatabaseAPI.StoreDatabase.Integration/Step1TableItemIntegration.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using DatabaseAPI.Factories;
+using DatabaseAPI;
+using DatabaseAPI.DatabaseModel;
+using DatabaseAPI.TableItem;
 using NUnit.Framework;
 
 namespace DatabaseAPI.StoreDatabase.Integration
@@ -17,8 +16,48 @@ namespace DatabaseAPI.StoreDatabase.Integration
         public void Setup()
         {   
             _db = new DatabaseService(new SqlStoreDatabaseFactory());
+
+            CleanUp();
         }
 
+        [TearDown]
+        public void Teardown()
+        {
+            CleanUp();
+        }
 
+        [Test]
+        public void TableItemGroup_CreateItemGroup_ItemGroupExistInDatabase()
+        {
+            // Creation
+            long createdItemGroupID = _db.TableItemGroup.CreateItemGroup("");
+        }
+
+        /*
+        [Test]
+        public void TableItem_CreateItem_ItemExistInDatabase()
+        {
+            // Creation
+            long createdItemID = _db.TableItem.CreateItem("Step1IntegrationTest_Item1", 0);
+
+            // Testing
+            Item fetchedItem = _db.TableItem.GetItem(createdItemID);
+
+            Assert.That(createdItemID, Is.EqualTo(fetchedItem.ItemID));
+        }
+        */
+
+        private void CleanUp()
+        {
+            // Cleanup item groups
+
+
+            // Cleanup items
+            List<Item> itemsToCleanUp = _db.TableItem.SearchItems("Step1IntegrationTest");
+            foreach (var item in itemsToCleanUp)
+            {
+                _db.TableItem.DeleteItem(item.ItemID);
+            }
+        }
     }
 }
